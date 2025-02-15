b4d9de048387d38644082886645d75f9c7d7799c
ThoNohT <e.c.p.bataille@gmail.com>
2025-02-15 16:14:56 +0100

    Don't hide the build output
    Since we can now sort-of parse the errorformat generated by gleam.

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -x
 
 if [ $1 = "build" ]; then
-    gleam build 2>/dev/null
+    gleam build
     esbuild js/index.mjs --bundle --outdir=dist --sourcemap --platform=browser
 
 elif [ $1 = "clean" ]; then
