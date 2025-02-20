51ae346d1f5c28832a2aed72521169299fb84a82
ThoNohT <e.c.p.bataille@gmail.com>
2025-02-20 17:06:09 +0100

    Don't sourcemap

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
 
 gleam build
-esbuild js/index.mjs --bundle --outdir=dist --sourcemap --platform=browser
+esbuild js/index.mjs --bundle --outdir=dist --platform=browser
 
--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -x
 
 if [ $1 = "build" ]; then
     gleam build
-    esbuild js/index.mjs --bundle --outdir=dist --sourcemap --platform=browser
+    esbuild js/index.mjs --bundle --outdir=dist --platform=browser
 
 elif [ $1 = "clean" ]; then
     rm -rf ./build
