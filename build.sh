a7d30f0635291d2cc45f286d1f497cbc158c92db
ThoNohT <e.c.p.bataille@gmail.com>
2025-02-28 16:30:45 +0100

    Add run command that opens the browser

--- build.sh
+++ build.sh
@@ -11,6 +11,9 @@ elif [ $1 = "clean" ]; then
     rm -f ./dist/*.js
     rm -f ./dist/*.js.map
 
+elif [ $1 = "run" ]; then
+    xdg-open http://localhost:5500
+
 elif [ $1 = "test" ]; then
     gleam test
 
